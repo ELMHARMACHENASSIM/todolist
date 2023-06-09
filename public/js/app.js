c76398b54baee1c446b271f5f0698987b3b871cb
NASSIM ELM'HARMACHE <48417897+ELMHARMACHENASSIM@users.noreply.github.com>
2023-06-09 16:51:58 +0100

    masalitoch

--- public/js/app.js
+++ public/js/app.js
@@ -3,10 +3,10 @@ let btnAdd = document.querySelector(".btnAdd");
 let notification = document.querySelector(".notification");
 let closeerror = document.querySelector(".closeerror");
 let listat = document.querySelector(".listat");
-let list = document.querySelector(".list");
-let selected = [];
-let notSelected = [];
-let mood = 'add';
+let lista = document.querySelector(".list");
+let btnSelect = document.querySelectorAll("selected");
+let completed = false;
+let mood = "add";
 let temp;
 
 //* create items
@@ -18,7 +18,7 @@ function createItems() {
   let newItem = {
     title: input.value,
   };
-  if (mood === 'add') {
+  if (mood === "add") {
     if (input.value == "") {
       notification.classList.add("dclose");
       const closeNot = () => {
@@ -30,11 +30,10 @@ function createItems() {
     }
   } else {
     items[temp].title = newItem.title;
-
-    mood = 'add';
+    mood = "add";
     const btna = `ADD
     <i class="fa-solid fa-circle-plus"></i>
-`
+`;
     btnAdd.innerHTML = btna;
   }
 
@@ -62,6 +61,24 @@ function showItem() {
     listat.innerHTML = list;
   }
 }
+
+//* select items
+
+let selected = [];
+let notSelected = [];
+let typeSelect = btnSelect.value;
+
+function selectItem(i) {
+  let liste = listat.children[i];
+  liste.classList.toggle("val");
+  selected.push(liste);
+  console.log(selected[i]);
+  console.log(typeSelect);
+
+}
+
+
+
 //! clear input
 function clearInput() {
   input.value = "";
@@ -82,91 +99,8 @@ function deletItem(i) {
 function updateItem(i) {
   input.value = items[i].title;
   btnAdd.innerHTML = "UPDATE";
-  mood = 'update';
+  mood = "update";
   temp = i;
- 
 }
-
-//* select items
-
-
-
-
-function selectItem(i){
-list.classList.add("val");
-  console.log(i);
-
-  showItem(); 
-}
-
 
 btnAdd.addEventListener("click", createItems);
-/* btnAdd.addEventListener("click", () => {
-    let item = input.value;
-    if (input.value == "") {
-        notification.classList.add("dclose")
-        const closeNot = () => {
-            notification.classList.remove("dclose")
-        }
-        closeerror.addEventListener("click", closeNot)
-    }
-    else {     
-        let listat = ""
-        for (let i = 0; i < items.length; i++) {
-            const item = items[i];
-            listat.innerHTML += `
-        <li class="list">
-        <h5>${i}</h5>
-        <h3>${item}</h3>
-        <div class="btns">
-            <!-- variables -->
-            <button ><i class=" validate fa-solid fa-circle-check "></i></button>
-            <button ><i class=" modItem fa-solid fa-pen"></i></button>
-            <button ><i onclick="modif(${item})" class=" deletItem deletItem fa-solid fa-trash-can "></i></button>
-        </div>
-    </li>
-        `; 
-        }
-       
-
-        input.value = "";
-        items.push(item);
-        console.log(items);
-
-
-
-    }
-
-
-
-})
-
-let validate = document.querySelectorAll(".validate");
-let modItem = document.querySelector(".modItem");
-let deletItem = document.querySelector(".deletItem");
-let list = document.querySelectorAll(".list");
-
-function select() {
-   alert("hello")
-}
-
-/* 
-document.addEventListener('click', function(ev) {
-  if (ev.target.className.includes("validate") ) {
-    ev.target.parentElement.parentElement.parentElement.classList.toggle('sele');
-    selected.push(ev.target.parentElement.parentElement.parentElement);
-    console.log(selected);
-  }
-
-
-});
-
-function modif() {
-alert("hello")
-}
-function delet() {
-
-}
-
-modItem.addEventListener("click",modif);
-deletItem.addEventListener("click",delet);  */
