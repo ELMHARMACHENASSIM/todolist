e9f0d6cf5acd5caef5b0bc0066199d4cfe8e7339
ELMHARMACHE NASSIM <nassim.nassim.raja@gmail.com>
2023-06-12 15:50:36 +0000

    select

--- public/js/app.js
+++ public/js/app.js
@@ -4,49 +4,52 @@ let notification = document.querySelector(".notification");
 let closeerror = document.querySelector(".closeerror");
 let listat = document.querySelector(".listat");
 let lista = document.querySelector(".list");
-let btnSelect = document.querySelectorAll("selected");
-let completed = false;
+let btnSelect = document.querySelector(".select");
 let mood = "add";
 let temp;
-
+input.addEventListener("keyup", (e) => {
+    if (e.keyCode == 13) {
+        createItems();
+    }
+})
 //* create items
 
 let items = [];
 
 function createItems() {
-  //* create object
-  let newItem = {
-    title: input.value,
-  };
-  if (mood === "add") {
-    if (input.value == "") {
-      notification.classList.add("dclose");
-      const closeNot = () => {
-        notification.classList.remove("dclose");
-      };
-      closeerror.addEventListener("click", closeNot);
+    //* create object
+    let newItem = {
+        title: input.value,
+    };
+    if (mood === "add") {
+        if (input.value == "") {
+            notification.classList.add("dclose");
+            const closeNot = () => {
+                notification.classList.remove("dclose");
+            };
+            closeerror.addEventListener("click", closeNot);
+        } else {
+            items.push(newItem);
+        }
     } else {
-      items.push(newItem);
-    }
-  } else {
-    items[temp].title = newItem.title;
-    mood = "add";
-    const btna = `ADD
+        items[temp].title = newItem.title;
+        mood = "add";
+        const btna = `ADD
     <i class="fa-solid fa-circle-plus"></i>
 `;
-    btnAdd.innerHTML = btna;
-  }
+        btnAdd.innerHTML = btna;
+    }
 
-  clearInput();
-  showItem();
+    clearInput();
+    showItem();
 }
 
 //* show item
 function showItem() {
-  let list = "";
-  for (let i = 0; i < items.length; i++) {
-    const element = items[i].title;
-    list += `
+    let list = "";
+    for (let i = 0; i < items.length; i++) {
+        const element = items[i].title;
+        list += `
          <li class="list">
          <h5>${i}</h5>
          <h3>${element}</h3>
@@ -58,49 +61,79 @@ function showItem() {
          </div>
      </li>
          `;
-    listat.innerHTML = list;
-  }
+        listat.innerHTML = list;
+    }
 }
 
 //* select items
 
-let selected = [];
-let notSelected = [];
-let typeSelect = btnSelect.value;
-
+let sel = [];
+let notSel = [];
 function selectItem(i) {
-  let liste = listat.children[i];
-  liste.classList.toggle("val");
-  selected.push(liste);
-  console.log(selected[i]);
-  console.log(typeSelect);
+    let liste = listat.children[i];
+    liste.classList.toggle("val");
+    if (liste.className.includes("val")) {
+        notSel.splice(i, 1)
+        sel.push(liste)
+        console.log(sel);
+    } else {
+        liste.classList.toggle("not_val");
+        sel.splice(i, 1)
+        notSel.push(liste);
+        console.log(notSel);
 
+    }
 }
+btnSelect.addEventListener("change", (e) => {
+    let targetSelect = e.target.value;
+    console.log(targetSelect);
+   
+
+    if (targetSelect == "selected") {
+        for (let i = 0; i < sel.length; i++) {
+            const elem = sel[i];
+            console.log(elem);
+
+        }
+
+    }
+    else if (targetSelect == "notSelected") {
+
+        for (let i = 0; i < sel.length; i++) {
+            const elemn = notSel[i];
+            console.log(elemn);
+        }
+
+    }
+    else {
+        showItem();
+    }
+});
 
 
 
 //! clear input
 function clearInput() {
-  input.value = "";
+    input.value = "";
 }
 
 //* delete items
 
 function deletItem(i) {
-  items.splice(i, 1);
-  if (items.length == 0) {
-    listat.innerHTML = "";
-  }
-  showItem();
+    items.splice(i, 1);
+    if (items.length == 0) {
+        listat.innerHTML = "";
+    }
+    showItem();
 }
 
 //* update items
 
 function updateItem(i) {
-  input.value = items[i].title;
-  btnAdd.innerHTML = "UPDATE";
-  mood = "update";
-  temp = i;
+    input.value = items[i].title;
+    btnAdd.innerHTML = "UPDATE";
+    mood = "update";
+    temp = i;
 }
 
 btnAdd.addEventListener("click", createItems);
